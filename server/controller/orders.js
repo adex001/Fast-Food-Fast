46543d9c5a8c9fbc51f871b30ca561c67139c66e
Olatunbosun Adeoye <adex001@gmail.com>
2018-09-28 10:27:59 +0100

    Implemented get specific order controller

--- server/controller/orders.js
+++ server/controller/orders.js
@@ -24,6 +24,38 @@ class OrderController {
       });
     });
   }
+
+  /**
+ * Get a specific order Controller retrieves a specific order
+ * @param {string} req - The request to the server
+ * @param {string} res - The response from the server.
+ */
+  static getSpecificOrder(req, res) {
+    let { ordersId } = req.params;
+    ordersId = parseInt(ordersId, 10);
+    const allOrdersQuery = `SELECT * FROM orders where ordersid = '${ordersId}'`;
+    pool.query(allOrdersQuery, (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'failed',
+          message: 'internal server error!',
+        });
+      }
+      if (result.rowCount < 1) {
+        return res.status(404).json({
+          status: 'failed',
+          message: 'Order not found',
+        });
+      }
+      if (result.rowCount > 0) {
+        return res.status(201).json({
+          status: 'success',
+          message: 'Order found',
+          data: result.rows[0],
+        });
+      }
+    });
+  }
 }
 
 export default OrderController;
