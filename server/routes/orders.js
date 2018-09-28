5ea5dcd22e493b04a24a4a62e8c43656a3e9035f
Olatunbosun Adeoye <adex001@gmail.com>
2018-09-28 10:00:34 +0100

    Implemented admin access on the getallorders route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import TokenHandler from '../middlewares/tokenHandler';
 
 import OrderController from '../controller/orders';
 
 const ordersRoute = Router();
 
-ordersRoute.get('/', OrderController.getAllOrders);
+ordersRoute.get('/', TokenHandler.verifyToken, TokenHandler.isAdmin, OrderController.getAllOrders);
 
 export default ordersRoute;
